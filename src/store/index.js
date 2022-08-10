6a2d9a2f3c755715ddaab269ff775093d3a41dd0
Shivansh Mehrotra <shivansh.sm1@gmail.com>
2022-08-11 03:04:57 +0530

    test

---
+++ src/store/index.js
@@ -0,0 +1,30 @@
+import { createStore } from "redux";
+
+const counterReducer = (state = { counter: 0 }, action) => {
+    if (action.type === "increment") {
+        return {
+            counter: state.counter + 1
+        }
+    }
+    if (action.type === "decrement") {
+        return {
+            counter: state.counter - 1
+        }
+    }
+    if (action.type === "IncrementBy5") {
+        return {
+            counter: state.counter + 5
+        }
+    }
+    if (action.type === "DecrementBy5") {
+        return {
+            counter: state.counter - 5
+        }
+    }
+    return state;
+};
+
+const store = createStore(counterReducer);
+
+
+export default store;
